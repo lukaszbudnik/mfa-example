8999d8e5e011129092d6076ff5d452f610a4c0a7
Łukasz Budnik <lukasz.budnik@gmail.com>
2020-10-09 11:53:37 +0200

    refreshed examples, made tests repeatable

--- src/test/java/com/github/lukaszbudnik/mfa/Rfc4226Test.java
+++ src/test/java/com/github/lukaszbudnik/mfa/Rfc4226Test.java
@@ -3,12 +3,10 @@ package com.github.lukaszbudnik.mfa;
 import net.glxn.qrgen.core.image.ImageType;
 import net.glxn.qrgen.javase.QRCode;
 import org.jboss.aerogear.security.otp.Totp;
-import org.jboss.aerogear.security.otp.api.Base32;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
 
 public class Rfc4226Test {
 
@@ -16,20 +14,24 @@ public class Rfc4226Test {
     private String testUser = "test-user-4226";
 
     @Test
-    public void test() throws IOException {
-        String secretKey = Base32.random();
+    public void test() {
+        // adding fixed secretKey so that test is repeatable
+        // or use org.jboss.aerogear.security.otp.api.Base32.random()
+        String secretKey = "IB6J7QGNUWGSWHY3";
 
         Totp totp = new Totp(secretKey);
 
         String uri = totp.uri(testUser) + "&issuer=" + application;
-
-        System.out.println(uri);
+        System.out.println("uri: " + uri);
 
         File file = QRCode.from(uri).to(ImageType.PNG).withSize(250, 250).file();
+        System.out.println("Open below file and configure your authenticator app");
+        System.out.println("file: " + file);
 
-        System.out.println("Open above file and configure Google Authenticator");
-
-        String verificationCode = "";
+        // in Unit test we get the code directly from Totp object
+        // check in your authenticator app that you have the same code
+        String verificationCode = totp.now();
+        System.out.println("verificationCode: " + verificationCode);
 
         boolean authorised = totp.verify(verificationCode);
 
--- src/test/java/com/github/lukaszbudnik/mfa/Rfc6238Test.java
+++ src/test/java/com/github/lukaszbudnik/mfa/Rfc6238Test.java
@@ -1,41 +1,47 @@
 package com.github.lukaszbudnik.mfa;
 
-import com.warrenstrange.googleauth.GoogleAuthenticator;
-import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
-import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
 import net.glxn.qrgen.core.image.ImageType;
 import net.glxn.qrgen.javase.QRCode;
+import org.cryptacular.codec.Base32Decoder;
+import org.cryptacular.generator.TOTPGenerator;
+import org.jboss.aerogear.security.otp.Totp;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 
 public class Rfc6238Test {
 
     private String application = "lukaszbudnik";
-    private String testUser = "test-user";
+    private String testUser = "test-user-6238";
 
     @Test
-    public void test() throws IOException {
+    public void test() {
+        // adding fixed secretKey so that test is repeatable
+        String secretKey = "PC5IVGTXGBW5KUK6";
 
-        GoogleAuthenticator gAuth = new GoogleAuthenticator();
+        Totp totp = new Totp(secretKey);
 
-        GoogleAuthenticatorKey key = gAuth.createCredentials();
+        String uri = totp.uri(testUser) + "&issuer=" + application;
+        System.out.println("uri: " + uri);
 
-        String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL(application, testUser, key);
+        File file = QRCode.from(uri).to(ImageType.PNG).withSize(250, 250).file();
+        System.out.println("Open below file and configure your authenticator app");
+        System.out.println("file: " + file);
 
-        System.out.println(url);
+        ByteBuffer bytes = ByteBuffer.allocate(10);
+        new Base32Decoder().decode(CharBuffer.wrap(secretKey), bytes);
 
-        String totpUrl = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL(application, testUser, key);
+        // in Unit test we get the code directly from TOTPGenerator object
+        // check in your authenticator app that you have the same code
+        TOTPGenerator generator = new TOTPGenerator();
+        // concatenate with empty string to convert the code to string
+        String verificationCode = "" + generator.generate(bytes.array());
+        System.out.println("verificationCode: " + verificationCode);
 
-        File file = QRCode.from(totpUrl).to(ImageType.PNG).withSize(250, 250).file();
-
-        System.out.println("Copy above url into browser or open above file and configure Google Authenticator");
-
-        int verificationCode = 0;
-
-        boolean authorised = gAuth.authorize(key.getKey(), verificationCode);
+        boolean authorised = totp.verify(verificationCode);
 
         Assert.assertTrue(authorised);
     }
